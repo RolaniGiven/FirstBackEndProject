f6bcbe79bf00999b40a26532c671cdb1384bca76
DA  M1 30 <dam1301@DAs-MacBook-Air-DA-M1-30.local>
2023-04-12 14:28:22 +0200

    first commit

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://RolaniDatabase:12345@tutorials.ybfxmfo.mongodb.net/?retryWrites=true&w=majority"
+  }; //mongodb+srv://RolaniDatabase:<password>@tutorials.ybfxmfo.mongodb.net/?retryWrites=true&w=majority
